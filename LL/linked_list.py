6c172e03ab4b2bd350819f9effcf92cbcf8da19a
Asad <71043942+asdhamidi@users.noreply.github.com>
2021-07-03 06:34:11 +0530

    Improving Documentation For Linked List

--- LL/linked_list.py
+++ LL/linked_list.py
@@ -1,55 +1,104 @@
+"""
+Code for Linked List.
+"""
 class Node:
     """
     Object Node
+    ...
+    Attributes:
+    ----------
+    data (int) : data for the node
+    next (Node) : Points to the next node
+
+    Methods:
+    -------
+    get_data(): returns data of the node
     """
     def __init__(self, data):
         """
-        Constructor functions
-        @params : data
+        Constructor function
+        parameters:
+        data (int) : data for the node.
         """
         self.data = data
         self.next = None
+
     def get_data(self):
         """
-        Getter function
-        @returns : data
+        Getter function for self.data
         """
         return self.data
 
 class LinkedList:
     """
     Object type : Linked List
+    ...
+    Attributes:
+    ----------
+    head (Node): Head of the LL.
+
+    Methods:
+    -------
+    get_head(): returns self.head
+    insert(data): inserts an element in the LL.
+    display(): displays the LL.
+    display_reverse(): displays the LL in reverse.
+    delete(data)/delete_recursive(head, data):
+        deletes an element in the LL, if found.
+    reverse()/reverse_recursive(): reverses the LL.
+    get_middle_element(): returns the middle element.
     """
     def __init__(self):
         self.head = None
 
-    def getHead(self):
+    def get_head(self):
+        """
+        Returns self.head
+        """
         return self.head
 
     def insert(self, data):
-        if self.head == None:
+        """
+        Inserts a element in the LL.
+
+        data(int): element to be inserted.
+        """
+        if not self.head:
             self.head= Node(data)
         else:
             current = self.head
-            while current.next != None:
+            while current.next is not None:
                 current = current.next
             current.next = Node(data)
 
     def display(self):
+        """
+        Displays the list.
+        """
         current = self.head
         while current:
             print(current.get_data(), end="->")
             current = current.next
 
     def display_reverse(self, head):
-        if head != None:
+        """
+        Displays the LL in reverse.
+
+        head(Node): head of the LL.
+        """
+        if head is not None:
             self.display_reverse(head.next)
         else:
             return
         print(head.getData(),end="->")
 
     def delete(self, data):
-        if self.head == None:
+        """
+        Deletes an element from the LL.
+
+        data(int): element to be deleted.
+        """
+        if self.head is None:
             print("Empty List")
             return
 
@@ -62,12 +111,18 @@ class LinkedList:
         while current.next.data != data:
             current = current.next
 
-        if current == None:
+        if current is None:
             print("Element Not found")
         else:
             current.next = current.next.next
 
-    def deleteRecursive(self, head, data):
+    def delete_recursive(self, head, data):
+        """
+        Deletes an element from the LL.
+
+        head(Node): head of the LL.
+        data(int): element to be deleted.
+        """
         if head is None:
             print("Element Not Found")
             return
@@ -84,19 +139,27 @@ class LinkedList:
                 prev.next = head.next
                 return
 
-        self.deleteRecursive(prev.next, data)
+        self.delete_recursive(prev.next, data)
 
     def reverse(self):
+        """
+        Reverses the LL.
+        """
         prev = None
         current = self.head
         while current is not None:
-            next = current.next
+            next_node = current.next
             current.next = prev
             prev = current
-            current = next
+            current = next_node
         self.head = prev
 
     def reverse_recursive(self, head):
+        """
+        Reverses the LL recursively.
+
+        head (Node): Head of the LL.
+        """
         if head is None or head.next is None:
             return head
 
@@ -108,22 +171,12 @@ class LinkedList:
         return rest
 
     def get_middle_element(self):
-        head = head1 = self.getHead()
-        while head1 != None and head1.next != None:
+        """
+        Returns the middle element of the LL.
+        """
+        head = head1 = self.get_head()
+        while head1 is not None and head1.next is not None:
             head1 = head1.next.next
             head = head.next
 
         print("Middle Element : ", head.data)
-
-if __name__ == "__main__":
-    q = LinkedList()
-    for r in range(1, 6):
-        q.insert(r)
-    print("Before reversing : ",end="")
-    q.display()
-    print()
-    q.head = q.reverse_recursive(q.getHead())
-    print("After Reversing : ",end="")
-    q.display()
-    print()
-    q.get_middle_element()
